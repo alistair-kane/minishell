192361bc23c0013d83bbca0ed6666600ac64a493
alkane <alistair@student.42.fr>
2022-04-09 02:55:47 +0200

    fixed 'never hit' reserved symbols..?

--- src/utils.c
+++ src/utils.c
@@ -4,17 +4,16 @@
 
 int	is_reserved_symbol(char *argument)
 {
+	if (ft_strncmp("<<", argument, 2) == 0)
+		return (1);
+	if (ft_strncmp(">>", argument, 2) == 0)
+		return (1);
 	if (argument[0] == '|')
 		return (1);
 	if (argument[0] == '<')
 		return (1);
 	if (argument[0] == '>')
 		return (1);
-	// these will never be hit !!!!!
-	/*if (ft_strncmp("<<", argument, 2) == 0)
-		return (1);
-	if (ft_strncmp(">>", argument, 2) == 0)
-		return (1);*/
 	return (0);
 }
 
