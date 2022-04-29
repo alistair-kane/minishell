d5790506e05f76b26fe0066d1c36b4f7080ba1d2
Alistair Kane <alkane@c2s18d119.42wolfsburg.de>
2022-04-29 19:07:57 +0200

    prints double prompt on cat, wc.  Fixed line clearing

--- src/signals.c
+++ src/signals.c
@@ -11,7 +11,7 @@ void	signal_handler(int sig)
 {
 	if (sig == SIGINT)
 	{
-		ft_putchar_fd('\n', STDOUT_FILENO);
+		rl_replace_line("", 0);		ft_putchar_fd('\n', STDOUT_FILENO);
 		rl_on_new_line();
 		rl_redisplay();
 		// todo !!!!! kill(child_pid, SIGKILL);
