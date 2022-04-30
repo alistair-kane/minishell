9a8107b01c51ea663a9c38e2c9d46b37a8786a67
Alistair Kane <alkane@c2s18d119.42wolfsburg.de>
2022-04-30 19:16:07 +0200

    updated cd file structure

--- src/builtins/cd_utils_2.c
+++ src/builtins/cd_utils_2.c
@@ -6,3 +6,18 @@ int	max(int val1, int val2)
 		return (val1);
 	return (val2);
 }
+
+char	*get_home_dir(t_data *data)
+{
+	size_t			i;
+	t_environment	*temp;
+
+	i = -1;
+	while (++i < data->environment->total)
+	{
+		temp = vector_get(data->environment, i);
+		if (!ft_strncmp(temp->name, "HOME", 4))
+			return (temp->value);
+	}
+	return (NULL);
+}
