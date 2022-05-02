af1238278091c80cdfda4a9499f3a92b59e99f68
Alistair Kane <alkane@c2s18d119.42wolfsburg.de>
2022-05-02 14:29:05 +0200

    grouped prototypes by filename

--- minishell.h
+++ minishell.h
@@ -75,24 +75,25 @@ typedef struct s_data
 	int			exit_value;
 }				t_data;
 
+// data_structs.c
 t_data	*data_init(void);
 
+// signals.c
+void	signal_handler(int signal);
+
+// data_cleanup.c
 void	data_cleanup(t_data *data);
 void	free_path(t_data *data);
 void	cleanup_environment(void *data);
 void	cleanup_exec(void *data);
 void	cleanup_exec_commands(void *data);
 
+// parser.c
 void	parser(t_data *data, char *buf);
-void	exec(t_data *data);
-void	exit_error(const char *s);
-void	open_pipe(int *fds);
-void	redirect_input(t_exec *exec);
-void	redirect_output(t_exec *exec, int redir_flag);
-void	close_ends(int *fds);
-void	exec_cmd(t_data *data, char **argv);
-int		check_parent_builtin(t_data *data, char **cmd, int exec);
+int		check_builtin(char **args);
+void	exec_builtin(t_data *data, char **args);
 
+// builtins/*.c
 int		builtin_echo(t_data *data, char **buf);
 void	builtin_cd(t_data *data, char **args);
 int		builtin_pwd(t_data *data);
@@ -100,43 +101,59 @@ int		builtin_export(t_data *data, char **args);
 int		builtin_unset(t_data *data, char **args);
 int		builtin_env(t_data *data, char **args);
 void	builtin_exit(t_data *data, char **args);
-
-char	**ms_split(t_data *data, char *line);
-int		check_binaries(t_data *data, char **args);
-int		check_builtin(char **args);
-void	exec_builtin(t_data *data, char **args);
-int		handle_quotes(char c, int *double_quotes, int *single_quotes);
-void	quote_master(char **args);
-
-int		is_reserved_symbol(char *argument);
-int		get_name_length(char *entry);
-int		get_env_var_end(char *entry);
-int		get_name_length_whitespace(char *entry);
-void	free_c_vector(char **vector);
-
+// cd_utils_1.c
+void	parse_cur_path(char *cur_path);
+// cd_utils_2.c
+int		max(int val1, int val2);
+char	*get_home_dir(t_data *data);
+// export_utils.c
+char	*remove_surrounding_quotes(char *input);
+int		get_entry_index(t_vector *env, char *name);
+void	update_existing_entry(t_vector *env, int index, char *new_value);
+int		get_new_initial_index(t_vector *env);
+int		is_valid_entry_name(char *name);
+// env_utils_1.c
 void	sort_all_entries(t_vector *env);
-char	*env_expansion(t_data *data, char *arg);
 void	add_to_envp(t_data *data, char *name, char *value);
+// env_utils_2.c
+char	*env_expansion(t_data *data, char *arg);
 
-void	signal_handler(int signal);
+// split.c
+char	**ms_split(t_data *data, char *line);
+// split_utils.c
+void	quote_master(char **args);
+int		handle_quotes(char c, int *double_quotes, int *single_quotes);
+int		handle_reserved_symbols(char **line);
 
+// logic_check.c
+int		check_argument_logic(char **args);
+
+// prep_exec.c
 int		prep_exec(t_data *data, char **arguments);
+// prep_exec_utils.c
 t_exec	*init_exec(void);
 char	*handle_here_doc(t_exec *exec, char *delimiter);
 char	*create_filename(t_exec *exec, char *filename);
 int		handle_output(t_exec *exec, char *filename, int append);
 int		handle_commands(t_exec *exec, char **arguments);
 
-int		check_argument_logic(char **args);
+// exec.c
+void	exit_error(const char *s);
+void	exec(t_data *data);
+void	exec_cmd(t_data *data, char **argv);
+// exec_utils_1.c
+void	open_pipe(int *fds);
+void	redirect_input(t_exec *exec);
+void	redirect_output(t_exec *exec, int redir_flag);
+void	close_ends(int *fds);
+//exec_utils_2.c
+int		check_parent_builtin(t_data *data, char **cmd, int exec);
 
-char	*remove_surrounding_quotes(char *input);
-int		get_entry_index(t_vector *env, char *name);
-void	update_existing_entry(t_vector *env, int index, char *new_value);
-int		get_new_initial_index(t_vector *env);
-int		is_valid_entry_name(char *name);
-
-void	parse_cur_path(char *cur_path);
-int		max(int val1, int val2);
-char	*get_home_dir(t_data *data);
+// utils.c
+int		is_reserved_symbol(char *argument);
+int		get_name_length(char *entry);
+int		get_name_length_whitespace(char *entry);
+int		get_env_var_end(char *entry);
+void	free_c_vector(char **vector);
 
 #endif
